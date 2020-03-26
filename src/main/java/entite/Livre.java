c09decd0379a72b88cf4572c09ef33eba8878123
Boukhemis Dib <boukhemis2011@gmail.com>
2020-03-26 14:20:02 +0100

    tpa2

---
+++ src/main/java/entite/Livre.java
@@ -0,0 +1,74 @@
+/**
+ * 
+ */
+package entite;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * @author boukh
+ *
+ */
+@Entity
+@Table( name="livre")
+public class Livre {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+	@Column(name = "titre")
+	private String titre;
+	@Column(name = "auteur")
+	private String auteur;
+	
+	public Livre() {
+	}
+
+	public Livre(String titre, String auteur) {
+		super();
+		this.titre = titre;
+		this.auteur = auteur;
+	}
+
+	public Livre(Integer id, String titre, String auteur) {
+		super();
+		this.id = id;
+		this.titre = titre;
+		this.auteur = auteur;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitre() {
+		return titre;
+	}
+	
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+	
+	public String getAuteur() {
+		return auteur;
+	}
+	
+	public void setAuteur(String auteur) {
+		this.auteur = auteur;
+	}
+
+	@Override
+	public String toString() {
+		return "Livre [id=" + id + ", titre=" + titre + ", auteur=" + auteur + "]";
+	}	
+	
+}
--- src/main/java/entite/Testjpa.java
+++ src/main/java/entite/Testjpa.java
@@ -3,6 +3,10 @@
  */
 package entite;
 
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import javax.persistence.*;
 
 /**
@@ -10,14 +14,32 @@ import javax.persistence.*;
  *
  */
 public class Testjpa {
+    private static final Logger LOGGER = Logger.getLogger(Testjpa.class.getName());
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("restaurant-pu");
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("bibliotheque-b");
         EntityManager entityManager = entityManagerFactory.createEntityManager();
+        
+     // select en fonction d'un id
+        Livre livre = entityManager.find(Livre.class, 1);
+        //Livre livre2 = entityManager.find(Livre.class, "titre de livre Vingt mille lieues sous les mers");
 
+        LOGGER.log(Level.INFO, "titre de livre " + livre.getTitre());
+        
+		// select en fonction d'un titre
+		TypedQuery<Livre> query = entityManager.createQuery("SELECT l FROM Livre l WHERE l.titre = 'Germinal'",
+				Livre.class);
+
+		Livre l = query.getSingleResult();
+
+		LOGGER.log(Level.INFO, "Select livre Germinal : {0}", l);
+
+        entityManager.close();
+
+        entityManagerFactory.close();
 	}
 
 }
